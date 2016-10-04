9d31f5bd599cd8a5d40c4b4d5a878268b06fefd3
hugo.roussaffa <geodatup@gmail.com>
2016-10-04 03:38:39 -0700

    custom error template

--- geodatup/settings/base.py
+++ geodatup/settings/base.py
@@ -106,5 +106,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'geodatup','static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
     ]
