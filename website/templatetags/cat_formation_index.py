42edbc29169b3744a8ff0b65edf3a7d5151d3cdd
Hugo Roussaffa <hugoroussaffa@gmail.com>
2016-11-15 18:18:45 +0100

    Liaison entre formation et catformation pages dans services

--- website/templatetags/cat_formation_index.py
+++ website/templatetags/cat_formation_index.py
@@ -8,9 +8,19 @@ register = template.Library()
 
 
 @register.inclusion_tag('section-formation.html',takes_context=True)
-def show_index(context, catFormation,slug):
+def show_cat_formation_other(context, catFormation,slug):
     request = context['request']
     catFormation_List = CatFormation.objects.exclude(slug=slug)
     titre = 'Les autres formations'
     
     return {'catFormation_List': catFormation_List, 'titre' : titre, 'request':request}
+
+
+
+@register.inclusion_tag('section-formation.html',takes_context=True)
+def show_cat_formation_index(context):
+    request = context['request']
+    catFormation_List = CatFormation.objects.all()
+    
+    
+    return {'catFormation_List': catFormation_List, 'request':request}
